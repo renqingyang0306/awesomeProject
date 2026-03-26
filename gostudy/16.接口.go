904b23305378bf5050544518f54a921800c46ba6
renqingyang <renqingyang.rqy@alibaba-inc.com>
2026-03-26 21:44:55 +0800

    更改2

--- gostudy/16.接口.go
+++ gostudy/16.接口.go
@@ -1,3 +1,39 @@
 package main
 
-func main() {}
+import "fmt"
+
+// Animal 定义一个animal的接口，它有唱，跳，rap的方法
+type Animal interface {
+	sing()
+	jump()
+	rap()
+}
+
+// Chicken 需要全部实现这些接口
+type Chicken struct {
+	Name string
+}
+
+func (c Chicken) sing() {
+	fmt.Println("chicken 唱")
+}
+
+func (c Chicken) jump() {
+	fmt.Println("chicken 跳")
+}
+func (c Chicken) rap() {
+	fmt.Println("chicken rap")
+}
+
+func sing(obj Animal) {
+	obj.sing()
+}
+
+func main() {
+	var animal Animal
+	animal = Chicken{"ik"}
+	animal.sing()
+	animal.jump()
+	animal.rap()
+	sing(animal)
+}
